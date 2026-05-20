289388a60e6c20197a7a656ee14c95fcb540d598
TranNghia <tnghia2003@gmail.com>
2026-05-20 23:02:58 +0700

    video 30

---
+++ src/component/learn/MyComponent.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export const MyComponent = () => {
+    return (
+        <>
+            <div>eric & hoidanit update</div>
+            <div className={"child"} style={{borderRadius: "10px"}}>
+                Child
+            </div>
+        </>
+    );
+}
